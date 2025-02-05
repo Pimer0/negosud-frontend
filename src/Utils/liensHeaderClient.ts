fe12db30eb91f27d78ea9f0770f33cb1c30754d1
Pimer0 <zefun74@gmail.com>
2025-02-05 21:33:56 +0100

    ajout du header client

---
+++ src/Utils/liensHeaderClient.ts
@@ -0,0 +1,18 @@
+export const liensHeaderClient = [
+    {
+        pathname: '/',
+        text: 'Accueil'
+    },
+    {
+        pathname: '/shop',
+        text: 'Shop'
+    },
+    {
+        pathname: '/blog',
+        text: 'Blog'
+    },
+    {
+        pathname: '/plus',
+        text: 'Plus'
+    },
+];
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { montserrat } from './fonts'
+import HeaderClient from "@/components/HeaderClient";
 
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body
         className={montserrat.className}
       >
+      <HeaderClient pathname={""} />
         {children}
       </body>
     </html>
