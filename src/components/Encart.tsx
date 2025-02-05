6b9ecec97538a7aaa8d08f1ae18fbccf657366f6
Pimer0 <zefun74@gmail.com>
2025-02-05 17:24:31 +0100

    fix composant Encart.tsx

--- src/components/Encart.tsx
+++ src/components/Encart.tsx
@@ -6,8 +6,8 @@ import {barlowCondensed} from "@/app/fonts";
 const Encart: React.FC<EncartProps> = ({children, titre, corps, customWidth}) => {
     return (
         <div className={`bg-white rounded p-4 m-4 ${customWidth || ''} h-[260px]`}>
-            <h2 className={`font-bold ${barlowCondensed.className}`}>{titre}</h2>
-            <p>{corps}</p>
+            <h2 className={`font-bold ${barlowCondensed.className} leading-none mb-3`}>{titre}</h2>
+            <p className={"mb-3"}>{corps}</p>
             {children}
         </div>
     );
