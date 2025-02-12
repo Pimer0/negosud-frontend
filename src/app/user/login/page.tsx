7dc285bc987a1753ab7cb788c3c1eb8da88fad3f
Pimer0 <zefun74@gmail.com>
2025-02-12 15:08:13 +0100

    connexion login admin et nouvelles images pour caroussel

---
+++ src/app/user/login/page.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+                        import EncartForm from "@/components/EncartForm";
+                        import InfoBulle from "@/components/infoBulle";
+                        import Bouton from "@/components/Bouton";
+                        import { useRouter } from "next/navigation";
+                        import { FormEvent, useState } from "react";
+                        import { FormStateLogin, SignupFormSchema } from "@/lib/zodDefinitions";
+                        import { loginClient } from "@/services/api/ClientAuth";
+
+                        export default function Login() {
+                            const router = useRouter();
+                            const [formData, setFormData] = useState({
+                                email: '',
+                                motDePasse: ''
+                            });
+
+                            const [errors, setErrors] = useState<FormStateLogin>({});
+                            const [success, setSuccess] = useState<boolean>(false);
+                            const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+                                const { name, value } = e.target;
+                                setFormData(prevState => ({
+                                    ...prevState,
+                                    [name]: value
+                                }));
+                            };
+
+                            // Validation des champs avec Zod
+                            const validatedFields = SignupFormSchema.safeParse({
+                                email: formData.email,
+                                motDePasse: formData.motDePasse,
+                            });
+
+                            const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+                                event.preventDefault();
+                                setErrors({});
+                                setSuccess(false);
+                                if (!validatedFields.success) {
+                                    setErrors({
+                                        errors: validatedFields.error.flatten().fieldErrors,
+                                        message: 'Veuillez corriger les erreurs ci-dessous.'
+                                    });
+                                    return;
+                                }
+
+                                try {
+                                    // Appel du service pour log un client
+                                    await loginClient({
+                                        email: formData.email,
+                                        motDePasse: formData.motDePasse
+                                    });
+
+                                    setSuccess(true);
+                                    // Rediriger l'utilisateur après une connexion réussie
+                                    router.back();
+                                } catch (error) {
+                                    setErrors({
+                                        message: 'Une erreur est survenue lors de la connexion.'
+                                    });
+                                    console.error(error);
+                                }
+                            }
+
+                            return (
+                                <EncartForm titre={"Connectez vous à votre espace administrateur"} customWidth={"w-[614px]"}>
+                                    <form onSubmit={onSubmit} className="mt-6">
+                                        <div className={"flex flex-col gap-4"}>
+                                            <label htmlFor="email">Email</label>
+                                            <input
+                                                type="email"
+                                                name="email"
+                                                id="email"
+                                                value={formData.email}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                            {errors?.errors?.email && (
+                                                <InfoBulle
+                                                    colorClass={"bg-[#FECACA] text-[#450A0A] border-[#450A0A]"}
+                                                    content={errors.errors.email[0]}
+                                                />
+                                            )}
+
+                                            <label htmlFor="motDePasse">Mot de passe</label>
+                                            <input
+                                                type="password"
+                                                name="motDePasse"
+                                                id="motDePasse"
+                                                value={formData.motDePasse}
+                                                onChange={handleChange}
+                                                required
+                                            />
+                                            {errors?.errors?.motDePasse && (
+                                                <InfoBulle
+                                                    colorClass={"bg-[#FECACA] text-[#450A0A] border-[#450A0A]"}
+                                                    content={errors.errors.motDePasse[0]}
+                                                />
+                                            )}
+
+                                            <Bouton
+                                                colorClass={"bg-[#1E4147] text-white"}
+                                                hoverColorClass={"hover:bg-white hover:text-[#1E4147]"}
+                                                text={"Connexion"}
+                                                customType="submit"
+                                            />
+                                        </div>
+                                    </form>
+                                </EncartForm>
+                            )
+                        }
