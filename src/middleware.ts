a0ebbbe3a5cbe2bb7057c91a8bcac1d84bc9c80f
Pimer0 <zefun74@gmail.com>
2025-03-07 20:22:47 +0100

    fix du midleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,37 +6,40 @@ import { SessionPayload } from '@/interfaces/SessionPayload';
 export async function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
 
-    // Exclure la route /user/login et /user/code
-    if (pathname === '/user/login' || '/user/code') {
+    // Cas spécial pour /user/code - toujours accessible
+    if (pathname === '/user/code') {
         return NextResponse.next();
     }
 
-    // Vérifier si l'utilisateur a le cookie `CookieCode` pour accéder à /user/login
+    // Vérification pour /user/login
     if (pathname === '/user/login') {
         const hasCookieCode = request.cookies.get('CookieCode')?.value === 'true';
         if (!hasCookieCode) {
-            // Rediriger vers la page de code si le cookie n'est pas présent
             return NextResponse.redirect(new URL('/user/code', request.url));
         }
         return NextResponse.next();
     }
 
+    // Pour toutes les autres routes protégées, vérifier l'authentification
     const session = request.cookies.get('session')?.value;
     const sessionUser = request.cookies.get('sessionUser')?.value;
 
     let payload: SessionPayload | null = null;
-    let isAdmin = false;
+    /*let isAdmin = false;*/
 
     if (session) {
         payload = await decrypt(session) as SessionPayload;
     } else if (sessionUser) {
         payload = await decrypt(sessionUser) as SessionPayload;
-        isAdmin = true;
+        /*isAdmin = true;*/
     }
 
     if (!payload || new Date(payload.exp * 1000) < new Date()) {
-        // Rediriger vers la page de connexion si l'utilisateur n'est pas authentifié
-        if (!isAdmin) {
+        // Vérifier si c'est une requête API (par exemple en vérifiant le chemin ou les en-têtes)
+        const isApiRequest = pathname.startsWith('/api/');
+
+        if (isApiRequest) {
+            // Pour les requêtes API, renvoyer un JSON
             return new NextResponse(
                 JSON.stringify({ message: 'Non autorisé' }),
                 {
@@ -44,9 +47,10 @@ export async function middleware(request: NextRequest) {
                     headers: { 'Content-Type': 'application/json' }
                 }
             );
+        } else {
+            // Pour les requêtes de page, rediriger vers la page unauthorized
+            return NextResponse.redirect(new URL('/unauthorized', request.url));
         }
-        // Pour les requêtes de page, rediriger vers la page unauthorized
-        return NextResponse.redirect(new URL('/unauthorized', request.url));
     }
 
 
